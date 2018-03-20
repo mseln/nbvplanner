87666952eb78ff5b2c7e37025f4105a3cae06e92
mseln <mange.selin@gmail.com>
2018-03-20 16:54:02 +0100

    Further port nbvplanner to rpl "framework"

--- interface_nbvp_rotors/src/rpl_exploration.cpp
+++ interface_nbvp_rotors/src/rpl_exploration.cpp
@@ -17,6 +17,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * TODO Check legal...
+ * Forked by Magnus Selin
+ * Copyright 2018 Magnus Selin, RPL, KTH, Sweden
  */
 
 #include <thread>
